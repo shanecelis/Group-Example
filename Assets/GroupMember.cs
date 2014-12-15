83d8fa999868b7c6d36dfdd45614105a629c6ddc
Shane Celis <shane.celis@gmail.com>
2014-12-15 15:56:20 -0500

    Initial import.

---
+++ Assets/GroupMember.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroupMember : MonoBehaviour {
+  public string groupName = "Group";
+
+	// Use this for initialization
+	void Start () {
+    Group group = Group.GetGroup(groupName);
+    group.Register(this);
+	}
+
+  void OnDestroy() {
+    Group.Deregister(this);
+  }
+}
---
+++ Assets/Replicator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Replicator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    Invoke("Replicate", 0.4f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  void Replicate() {
+    GameObject go = Instantiate(this.gameObject,
+                                transform.position + Vector3.right * 1.2f,
+                                Quaternion.identity) as GameObject;
+    go.name = this.gameObject.name;
+    go.transform.parent = this.transform.root;
+  }
+}
